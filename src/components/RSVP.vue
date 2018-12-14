bf30c472ae256323aa58a226054d029d436c7274
Henry Bi <henrybi@ripl.com>
2018-12-13 23:10:36 -0800

    added step 2

--- src/components/RSVP.vue
+++ src/components/RSVP.vue
@@ -31,9 +31,11 @@
                              @change="checkGuestPartnerStatus()"></md-switch>
                 </div>
                 <div class="md-layout-item md-large-size-66 partner" v-if="selectedGuest">
-                  <md-checkbox class="md-primary" v-model="selectedGuestAttending" v-if="selectedGuest"><b>{{ selectedGuest.full_name}}</b>
+                  <md-checkbox class="md-primary" v-model="selectedGuestAttending" v-if="selectedGuest" :disabled="guestsAttending"><b>{{
+                                                                                                                                       selectedGuest.full_name}}</b>
                   </md-checkbox>
-                  <md-checkbox class="md-primary" v-model="selectedGuestPartnerAttending" v-if="selectedGuestPartnerIncluded"><b>{{selectedGuestPartner.full_name}}</b>
+                  <md-checkbox class="md-primary" v-model="selectedGuestPartnerAttending" v-if="selectedGuestPartnerIncluded"
+                               :disabled="guestsAttending"><b>{{selectedGuestPartner.full_name}}</b>
                   </md-checkbox>
                 </div>
                 <div class="md-layout-item md-large-size-33" v-if="selectedGuest">
@@ -47,18 +49,102 @@
               <Confetti v-if="guestsAttending"/>
             </md-step>
 
-            <md-step id="second" md-label="Second Step" :md-error="secondStepError" :md-editable="false" :md-done.sync="second">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo
-                 eius
-                 amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
-              <md-button class="md-raised md-primary" @click="setDone('second', 'third')">Continue</md-button>
-              <md-button class="md-raised md-primary" @click="setError()">Set error!</md-button>
+            <md-step id="second" md-label="Book Your Travel!" :md-editable="false" :md-done.sync="second">
+              <div class="md-layout">
+                <div class="md-layout-item md-medium-size-100" v-if="selectedGuest || selectedGuestPartner ">
+                  <b v-if="selectedGuestAttending">{{selectedGuest.full_name}}</b>
+                  <b v-if="selectedGuestAttending && selectedGuestPartnerAttending"> & </b>
+                  <b v-if="selectedGuestPartnerAttending">{{selectedGuestPartner.full_name}}</b>
+                </div>
+                <div class="md-layout-item md-medium-size-50">
+                  <div class="question1"><b>Have you booked the Resort?</b></div>
+                  <div class="question2"><b>Have you booked your Flights?</b></div>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <div>
+                    <md-switch v-model="selectedGuest.resort_booked" class="md-primary questionSwitch1" v-if="selectedGuest">
+                      <span v-if="!selectedGuest.resort_booked">Not Yet</span> <span v-if="selectedGuest.resort_booked">Sure Did!</span>
+                    </md-switch>
+                  </div>
+                  <div>
+                    <md-switch v-model="selectedGuest.flight_booked" class="md-primary questionSwitch2" v-if="selectedGuest">
+                      <span v-if="!selectedGuest.flight_booked">Not Yet</span> <span v-if="selectedGuest.flight_booked">Heck Yah!</span>
+                    </md-switch>
+                  </div>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <md-datepicker v-model="selectedGuest.resort_arrival_date"
+                                 v-if="selectedGuest">
+                    <label>Arrival Date</label>
+                  </md-datepicker>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <md-datepicker v-model="selectedGuest.resort_departure_date"
+                                 v-if="selectedGuest">
+                    <label>Departure Date</label>
+                  </md-datepicker>
+                </div>
+              </div>
+
+              <md-button class="md-raised md-accent" @click="setDone('second', 'third')">Continue</md-button>
+
             </md-step>
-            <md-step id="third" md-label="Third Step" :md-editable="false" :md-done.sync="third">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed, temporibus nemo
-                 eius
-                 amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
-              <md-button class="md-raised md-primary" @click="setDone('third')">Done</md-button>
+            <md-step id="third" md-label="Other Info" :md-editable="false" :md-done.sync="third">
+              <div class="md-layout">
+                <div class="md-layout-item md-medium-size-100">
+                  <p class="updateInfo">Would you please update the following information So we can keep you posted on the best flight prices and
+                                        other
+                                        notifications</p>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <B v-if="selectedGuest"> {{ selectedGuest.full_name }}</B>
+                  <md-field v-if="selectedGuest">
+                    <label>Phone</label>
+                    <md-input v-model=selectedGuest.phone></md-input>
+                  </md-field>
+                  <md-field v-if="selectedGuest">
+                    <label>Email</label>
+                    <md-input v-model=selectedGuest.email></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <B v-if="selectedGuestPartner"> {{ selectedGuestPartner.full_name }}</B>
+                  <md-field v-if="selectedGuestPartner">
+                    <label>Phone</label>
+                    <md-input v-model=selectedGuestPartner.phone></md-input>
+                  </md-field>
+                  <md-field v-if="selectedGuestPartner">
+                    <label>Email</label>
+                    <md-input v-model=selectedGuestPartner.email></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <md-field v-if="selectedGuest">
+                    <label>Food</label>
+                    <md-input v-model=selectedGuest.food_allergies></md-input>
+                  </md-field>
+                  <md-field v-if="selectedGuest">
+                    <label>Events</label>
+                    <md-input v-model=selectedGuest.events></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <md-field v-if="selectedGuestPartner">
+                    <label>Food Allergies</label>
+                    <md-input v-model=selectedGuestPartner.food_allergies></md-input>
+                  </md-field>
+                  <md-field v-if="selectedGuestPartner">
+                    <label>Events</label>
+                    <md-input v-model=selectedGuestPartner.events></md-input>
+                  </md-field>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <md-button class="md-raised md-primary" @click="saveOtherInfo()">Save</md-button>
+                </div>
+                <div class="md-layout-item md-large-size-50">
+                  <md-button class="md-raised md-accent" @click="setDone('third')">Done</md-button>
+                </div>
+              </div>
             </md-step>
           </md-steppers>
         </md-card-content>
@@ -134,7 +220,12 @@
         }
       },
       rsvp() {
-        this.guestsAttending = true;
+        if ( this.selectedGuestAttending || this.selectedGuestPartnerAttending ) {
+          this.guestsAttending = true;
+        } else {
+          //TODO: create Dialog or message to people who are not attending
+        }
+
         store.dispatch('UPDATE_GUEST_BY_ID', {
           id: this.selectedGuest.id,
           attending: this.selectedGuestAttending,
@@ -148,8 +239,17 @@
       },
       setDone(id, index) {
         this[id] = true;
+        if ( index === 'second' ) {
+          if ( !this.selectedGuestAttending && this.selectedGuestPartnerAttending ) {
+            this.selectedGuest = this.selectedGuestPartner;
+          }
+          console.log('GUEST', this.selectedGuest, 'GUESTPARTNER', this.selectedGuestPartner);
+        }
 
-        this.secondStepError = null;
+        if ( index === 'third' ) {
+
+        }
+        ;
 
         if ( index ) {
           this.active = index;
@@ -194,8 +294,26 @@
       margin-top: 5px;
     }
 
-    button.md-button md-icon-button md-dense md-input-action md-clear md-theme-default {
-      display: none;
+    .question1 {
+      margin-top: 12px;
+      text-align: left;
+    }
+
+    .question2 {
+      margin-top: 15px;
+      text-align: left;
+    }
+
+    .questionSwitch1 {
+      margin-top: 12px;
+    }
+
+    .questionSwitch2 {
+      margin-top: 0px;
+    }
+
+    .updateInfo {
+      text-align: left;
     }
   }
 </style>
