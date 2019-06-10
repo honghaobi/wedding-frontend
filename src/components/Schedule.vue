508b2db671cb3e778f10962b8f3deae2543ed549
Henry Bi <henrybi@ripl.com>
2019-06-09 22:56:32 -0700

    updated date on schedule

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="block">
     <h1>Henry + Kelli Fiesta Weekend Schedule</h1>
-    <h2>Wednesday June 17th</h2>
+    <h2>Wednesday June 19th</h2>
     <h3>Rehearsal Dinner with Bridal Party and Immediate Families</h3>
     <li><span class="time">5:45PM</span> Wedding walkthrough on the beach</li>
     <li><span class="time">6:30PM</span> Dinner at BokaDos Steakhouse Restaurant</li>
@@ -9,12 +9,12 @@
     <li><span class="time">10:00PM</span> Transportation will be provided to leave Garza Blanca</li>
     <li>El Squid Roe & Mandala Los Cabos…</li>
     <li>We did not book transportation back to the resort. Uber or Taxi will be your best option</li>
-    <h2>Thursday June 18th</h2>
+    <h2>Thursday June 20th</h2>
     <h3>Pool Party on the Rooftop</h3>
     <li><span class="time">2:00-5:00PM</span></li>
     <h3>Meet and Greet Cocktails and Appetizers</h3>
     <li><span class="time">7:30-8:30PM</span> @ the Orange Tree</li>
-    <h2>Friday June 19th</h2>
+    <h2>Friday June 21th</h2>
     <h3>Wedding Day</h3>
     <li><span class="time">4:30PM:</span> Transportation to leave the resort for immediate family and significant others</li>
     <li><span class="time">5:00PM:</span> Transportation to leave the resort for all other guests</li>
@@ -22,7 +22,7 @@
     <li><span class="time">7:30PM:</span> Reception</li>
     <li><span class="time">1:00AM:</span> Last Song</li>
     <li>Shuttle will be leaving periodically throughout the night</li>
-    <h2>Saturday June 20th</h2>
+    <h2>Saturday June 22th</h2>
     <h3>Tropical Catamaran Day Cruise</h3>
     <li><span class="time">1:30PM:</span> Transportation will be provided to leave resort</li>
     <li><span class="time">2:00-6:00PM:</span> Catamaran Cruise</li>
